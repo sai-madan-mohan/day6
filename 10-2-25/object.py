b3944a76049403fecec1601d1941ff4bbfd6b9cd
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-02-10 15:20:46 +0530

    inheritance methods using class and object

--- 10-2-25/object.py
+++ 10-2-25/object.py
@@ -19,3 +19,22 @@
 # print(account.getbalance())
 
 # print(account.withdraw(10000))
+
+
+# inheritance class
+
+# class vehicle:
+#     def __init__(self,brand,speed):
+#         self.brand = brand
+#         self.speed = speed
+#     def showinfo(self):
+#         return f" {self.brand} runs with {self.speed}km/h"
+# class car(vehicle):
+#     def __init__(self,brand,speed,fueltype):
+#         super().__init__(brand,speed)
+#         self.fueltype = fueltype
+#     def carinfo(self):
+#         return f" {self.brand} runs with {self.speed}km/h uses {self.fueltype}"
+# c1 = car("Tesla",150,"Electric") 
+# print(c1.showinfo())
+# print(c1.carinfo())      
