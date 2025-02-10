8a08a71ea94642370baabdc95892f3a80f3ebf50
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-02-10 16:58:38 +0530

    dict problems

---
+++ 10-2-25/dict.py
@@ -0,0 +1,12 @@
+student1 = {"name": "sai","age":22,"grade":"A"}
+student2 = dict(name="madan",age=23,grades="B")
+# print(student1)
+# print(student2)
+# print(student2.get("name"))
+# print(student1.get("name"))
+
+student2["grades"]= "A"
+
+# print(student2)
+
+print(student1.items())
