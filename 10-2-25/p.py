b099b6b370432fc5d59f14d33f701279ba19262b
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-02-10 12:18:20 +0530

    list using wshile loop

--- 10-2-25/p.py
+++ 10-2-25/p.py
@@ -8,9 +8,24 @@
 # import math
 # print(math.floor(3.8))
 
-list1 = ["a", "b", "c"]
-list2 = [1, 2, 3]
-list4 = ["sai",23,3.5]
+# list1 = ["a", "b", "c"]
+# list2 = [1, 2, 3]
+# list4 = ["sai",23,3.5]
 
-list3 = list1 + list2+list4
-print(list3)
+# list3 = list1 + list2+list4
+# print(list3)
+mylist = ['apple', 'banana', 'cherry']
+i = 0
+while i < len(mylist):
+  print(mylist[i])
+  i = i + 1
+
+
+
+
+# list1 = ["a", "b" , "c"]
+# list2 = [1, 2, 3]
+
+# for x in list2:
+#     list1.append(x)
+# print(list1)
