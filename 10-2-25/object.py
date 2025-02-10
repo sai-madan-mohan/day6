12f06d3cb2afbebbf111576c9312bf8b8bf831d3
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-02-10 15:01:42 +0530

    bankaccount problem related class and object

---
+++ 10-2-25/object.py
@@ -0,0 +1,21 @@
+# class bankaccount:
+#     def __init__(self,name,balance):
+#         self.name = name
+#         self.balance = balance
+    
+#     def deposit (self,amount):
+#         self.balance= self.balance+ amount
+#     def withdraw(self,amount):
+#         self.balanace= self.balance-amount
+#         return f" wirhdrawn amount is {amount},Remaining balance is {self.balance}"
+#     def getbalance(self):
+#         return self.balance
+    
+    
+# account = bankaccount("sai",10000)
+# print(account.getbalance())
+
+# account.deposit(5000)
+# print(account.getbalance())
+
+# print(account.withdraw(10000))
