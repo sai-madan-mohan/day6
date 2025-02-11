ea5c9226fe975a85d06127badeec8bfab5b49072
Sai Madan Mohan <chalamalasetti27@gmail.com>
2025-02-11 14:34:24 +0530

    function methods

--- 11-2-25/todaywork.py
+++ 11-2-25/todaywork.py
@@ -1,5 +1,20 @@
-def greet(name):
-    print(f"happy bday {name}")
-greet("Sai")
-greet("Madan")
-greet("Mohan")
+# def greet(name):
+#     print(f"happy bday {name}")
+# greet("Sai")
+# greet("Madan")
+# greet("Mohan")
+
+# def createname(fname,lname):
+#     fname= fname.capitalize()
+#     lname = lname.capitalize()
+#     return fname + " " + lname
+# print(createname("sai","madan"))
+
+# def add(x,y=2):
+#     print(x+y)
+# print(add(4))
+# print(add(3,5))
+# print(add(3,2))
+
+# print(add(2,2))
+# print(add(4,2))
